469792f66093a9855121dbc4c6901aa9920ef3a9
Emerson <emerson.garcia@obxd.com.br>
2017-03-07 21:22:57 -0300

    Primeiro Commit
    Primeiro commit da loja

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Bungee|Lato" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Husky On Live Commerce</title>
+    </head>
+
+    <body>
+        <!-- Topo principal da loja -->
+        <header id="topo-principal">
+          <!-- Parte com os principais links úteis de acesso da loja -->
+          <article id="top-links-infos">
+            <div class="tli-flex">
+              <ul>
+                <li><a href="#">Lista de Desejos</a></li>
+                <li><a href="#">Atendimento</a></li>
+                <li><a href="#">Meu Carrinho</a></li>
+              </ul>
+            </div>
+
+            <!-- Data e hora atual -->
+            <div id="date-time">
+              <a href="#" class="date-time">Hoje, 17h35, São Paulo, 07 de Março de 2017.</a>
+            </div>
+          </artcile>
+
+          <!-- Parte com o logo e menu -->
+          <figure id="logo-menu-infos">
+            <div id="logo-flex-area">
+              <img src="img/logo.png" class="logo">
+              <a href="#" class="menu-modular">Sobre Nós</a>
+            </div>
+          </figure>
+
+          <!-- Informações Importantes -->
+          <article id="import-infos">
+          </artcile>
+
+          <!-- Search -->
+          <article id="search">
+          </artcile>
+
+        </header>
+
+    </body>
+</html>
