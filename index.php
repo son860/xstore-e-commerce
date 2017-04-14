c2bb30282d8cee6838d1b57913e45ec68bf5f862
Emerson <emerson.garcia@obxd.com.br>
2017-04-14 20:45:24 -0300

    Segundo commit
    Segundo commit da loja virtual

--- index.php
+++ index.php
@@ -23,27 +23,216 @@
 
             <!-- Data e hora atual -->
             <div id="date-time">
-              <a href="#" class="date-time">Hoje, 17h35, São Paulo, 07 de Março de 2017.</a>
+              <a href="#" class="date-time">Hoje, 07 de Março de 2017.</a>
             </div>
           </artcile>
 
           <!-- Parte com o logo e menu -->
           <figure id="logo-menu-infos">
-            <div id="logo-flex-area">
+            <div id="logo-flex-area"  class="tli-flex">
               <img src="img/logo.png" class="logo">
               <a href="#" class="menu-modular">Sobre Nós</a>
+              <p class="text-call">Olá visitnte! <a href="#" class="text-call-link">Cadastre-se</a> ou faça <a href="#" class="text-call-link">login.</a></p>
             </div>
           </figure>
 
           <!-- Informações Importantes -->
-          <article id="import-infos">
-          </artcile>
+          <figure id="import-infos">
+            <div id="infos-area" class="tli-flex">
+              <img src="img/img-infos.png" class="img-infos">
+            </div>
+          </figure>
 
           <!-- Search -->
           <article id="search">
+            <div id="search-area" class="tli-flex">
+              <input type="search" class="search" placeholder="O que procura?">
+              <a href="#" class="btn-search">OK</a>
+            </div>
           </artcile>
 
-        </header>
+          <!-- Slider -->
+          <figure id="slider">
+            <!-- Slide Group 1 -->
+            <div id="slide-group-1">
+              <a href="#"><img src="img/slider-1.png" class="img-slider"></a>
+            </div>
+
+            <!-- Slide Group 2 -->
+            <div id="slide-group-2">
+
+            </div>
+
+            <!-- Slide Group 3 -->
+            <div id="slide-group-1">
+
+            </div>
+          </figure>
+
+      </header>
+
+      <!-- THUMBS BUSCA RÁPIDA -->
+      <section id="busca-rapida-icon">
+        <div id="busca-icons" class="tli-flex">
+            <img src="img/b-icons-1.png" class="busca-icons">
+            <img src="img/b-icons-2.png" class="busca-icons">
+
+            <video id="video">
+            </video>
+        </div>
+      </section>
+
+      <!-- INFOS PRINCIPAL GROUP -->
+      <section id="infos-principal-group" class="tli-flex">
+        <!-- MENU menu-categorias -->
+        <aside id="menu-categorias">
+
+          <div id="menu-categorias-title">
+            <p class="menu-categortias-title">Categorias</p>
+          </div>
+
+          <nav id="menu-categorias">
+             <ul>
+               <li><a href="#">Notebooks</a></li>
+               <li><a href="#">Ultrabooks</a></li>
+               <li><a href="#">Tablets</a></li>
+               <li><a href="#">Periféricos</a></li>
+               <li><a href="#">Fontes</a></li>
+               <li><a href="#">Bloqueadores</a></li>
+             </ul>
+          </nav>
+        </aside>
+        <!-- Fim Menu Categorias -->
+
+        <!-- Buscar produto -->
+        <aside id="buscar-produto">
+
+          <div id="buscar-produto-title">
+            <p class="buscar-produto-title">Buscar Produto Por</p>
+          </div>
+
+          <nav id="buscar-produto">
+             <ul>
+               <li><a href="#">Marca</a></li>
+               <li><a href="#">Ano</a></li>
+               <li><a href="#">Novo</a></li>
+               <li><a href="#">Usado</a></li>
+               <li><a href="#">Semi-Novo</a></li>
+               <li><a href="#">Notebook</a></li>
+               <li><a href="#">Ultrabook</a></li>
+             </ul>
+          </nav>
+        </aside>
+        <!-- Fim Menu Categorias -->
+
+
+        <article id="produtos-destaque">
+          <div id="produtos-destaque-title">
+            <p class="pd-title">Produtos em Destaque</p>
+          </div>
+
+          <!-- Products Single 1 -->
+          <div id="products-single-1">
+            <figure id="product-s-image-1">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-1">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-1">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 1 -->
+
+          <!-- Products Single 2 -->
+          <div id="products-single-2">
+            <figure id="product-s-image-2">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-2">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-2">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 2 -->
+
+          <!-- Products Single 3 -->
+          <div id="products-single-3">
+            <figure id="product-s-image-3">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-3">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-3">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 3 -->
+
+          <!-- Products Single 4 -->
+          <div id="products-single-4">
+            <figure id="product-s-image-4">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-4">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-1">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 4 -->
+
+          <!-- Products Single 5 -->
+          <div id="products-single-5">
+            <figure id="product-s-image-5">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-5">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-5">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 5 -->
+
+          <!-- Products Single 6 -->
+          <div id="products-single-6">
+            <figure id="product-s-image-6">
+              <img src="img/lenovo.png" class="img-product">
+            </figure>
+
+            <p class="ps-description-6">Dell Vostro<br>Semi Novo</p><br>
+            <p class="ps-description-price-6">R$ 1.500,00<br>Ou 3x sem juros</p>
+
+            <div id="buttons-buy">
+              <a href="#" class="btn-comprar">Comprar</a>
+              <a href="#" class="btn-detalhes">Detalhes</a>
+            </div>
+          </div>
+          <!-- Fim Products Single 5 -->
+
+
+        </article>
+
+      </section>
+
+
 
     </body>
 </html>
